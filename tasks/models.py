d0fe95e3762c8b9c5ba038c4c0f0b183dab818ed
xenkidu <aburton85@live.com>
2020-06-27 10:38:32 -0500

    Created the Model: Task. Added Task to admin.site."

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.TextField(max_length=80)
+    estimated_time = models.IntegerField()
+    actual_time = models.IntegerField()
+    is_recurring = models.BooleanField()
+
+    def __str__(self):
+        return self.name
