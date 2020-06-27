b2240238a1fa2fb12a6af5a0edb9714b89994f73
xenkidu <aburton85@live.com>
2020-06-27 14:05:49 -0500

    updated Task model

--- tasks/models.py
+++ tasks/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Task(models.Model):
-    name = models.TextField(max_length=80)
+    name = models.CharField(max_length=80)
     estimated_time = models.IntegerField()
-    actual_time = models.IntegerField()
-    is_recurring = models.BooleanField()
+    actual_time = models.IntegerField(null=True, blank=True)
+    is_recurring = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
